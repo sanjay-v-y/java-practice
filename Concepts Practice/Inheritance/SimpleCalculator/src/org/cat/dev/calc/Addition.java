752b7e1414b4789ded51c7d49f9394940005db43
Sanjay V Y <167050358+sanjay-v-y@users.noreply.github.com>
2025-05-06 15:11:19 +0530

    hierarchical inheritance

---
+++ Concepts Practice/Inheritance/SimpleCalculator/src/org/cat/dev/calc/Addition.java
@@ -0,0 +1,13 @@
+package org.cat.dev.calc;
+
+public class Addition extends Operations {
+	
+	public void sumVal(int num1, int num2) {
+		
+		Operations operSum = new Operations();
+		
+		operSum.add(num1, num2);
+
+	}
+	
+}
---
+++ Concepts Practice/Inheritance/SimpleCalculator/src/org/cat/dev/calc/Operations.java
@@ -0,0 +1,14 @@
+package org.cat.dev.calc;
+
+public class Operations {
+
+	protected void add(int num1, int num2) {
+		System.out.println(num1 + " + " + num2 + " = " + (num1+num2));
+	}
+	
+	protected void multiply(int num1, int num2) {
+		System.out.println(num1 + " x " + num2 + " = " + (num1*num2));
+
+	}	
+	
+}
