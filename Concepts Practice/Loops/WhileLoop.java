fee992933a0b23114a42486a927302b0eb693787
Sanjay V Y <167050358+sanjay-v-y@users.noreply.github.com>
2025-05-12 19:16:21 +0530

    while loop

---
+++ Concepts Practice/Loops/WhileLoop.java
@@ -0,0 +1,18 @@
+package org.hex.dev.loops;
+
+public class WhileLoop {
+	
+	public static void main(String[] args) {
+		
+		int i=10;
+		
+		System.out.println("While Loop:");
+		
+		while(i>0) {
+			
+			System.out.println(i);
+			
+			i--;
+		}
+	}
+}
