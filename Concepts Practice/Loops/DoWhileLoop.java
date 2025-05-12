a55d29a8952458e4a6203c432051b4b734d546ee
Sanjay V Y <167050358+sanjay-v-y@users.noreply.github.com>
2025-05-12 19:15:08 +0530

    do while

---
+++ Concepts Practice/Loops/DoWhileLoop.java
@@ -0,0 +1,19 @@
+package org.hex.dev.loops;
+
+public class DoWhileLoop {
+	
+	public static void main(String[] args) {
+		
+		int i=10;
+		
+		System.out.println("Do While Loop:");
+		
+		do {
+			
+			System.out.println(i);
+			
+			i--;
+			
+		} while (i>0);
+	}
+}
