67065bc3efea670c2413e7fb9056e27ea69b63b3
Sanjay V Y <167050358+sanjay-v-y@users.noreply.github.com>
2025-05-12 19:15:56 +0530

    nested for

---
+++ Concepts Practice/Loops/NestedFor.java
@@ -0,0 +1,19 @@
+package org.hex.dev.loops;
+
+public class NestedFor {
+	
+	public static void main(String[] args) {
+		
+		for (int i = 0; i < 1; i++) {
+			
+			System.out.println("Nested For Loop:");
+			
+			for (int j = 10; j > 0; j--) {
+				
+				System.out.println(j);
+				
+			}
+			
+		}
+	}
+}
