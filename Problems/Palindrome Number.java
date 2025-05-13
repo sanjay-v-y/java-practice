d3a47b5544cc5a678530427a24dc38e4b23977a5
Sanjay V Y <167050358+sanjay-v-y@users.noreply.github.com>
2025-05-13 20:23:27 +0530

    better code

--- Problems/Palindrome Number.java
+++ Problems/Palindrome Number.java
@@ -1,42 +1,20 @@
 public class Palindrome Number {
     public boolean isPalindrome(int x) {
         
-        boolean isPalindromeNum;
-
         if (x<0){
-
-            isPalindromeNum = false;
-            return isPalindromeNum;
+            return false;
         }
 
         int num = x;
         int revNum=0;
-        int remainder = num % 10;
-        int tenthVal=1;
 
-        while(num>=10){
+        while( num > 0 ){
 
-            remainder = num % 10;
-
-            revNum = (revNum * tenthVal) + remainder;
-
-            num = num / 10;
-
-            tenthVal=10;
+            revNum = (revNum*10) + (num%10);
+            num/=10;
 
         }
 
-        revNum = (revNum * 10) + num;
-
-        System.out.println(revNum);
-
-        if(revNum == x){
-            isPalindromeNum = true;
-        }
-        else{
-            isPalindromeNum = false;
-        }
-
-        return isPalindromeNum;
+        return revNum == x;
     }
 }
